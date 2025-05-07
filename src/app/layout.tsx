cbae3b46d22ddc22f72a8cd8c24ccf06615d2017
Dario Kolic <work@dariokolic.com>
2025-05-07 22:00:42 +0200

    Added subsets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,8 @@ import { Cursor } from "@/components/Cursor/Cursor";
 import BackgroundAnimation from "@/components/BackgroundAnimation/BackgroundAnimation";
 
 const sourceCodePro = Source_Code_Pro({
-  weight: ['200', '300', '400', '500', '600', '700', '800', '900']
+  weight: ['200', '300', '400', '500', '600', '700', '800', '900'],
+  subsets: ['latin', 'cyrillic']
 });
 export const metadata: Metadata = {
   title: {
