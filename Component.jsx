f39b17a4e8e37038df51bb1b7cdcf5112372256b
Dario Kolic <work@dariokolic.com>
2025-05-12 15:36:42 +0200

    Updated keywords

---
+++ Component.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import './Expenses.css';
+
+const Expenses = ({ items }) => {
+  const [selectedYear, setSelectedYear] = useState('2020');
+
+  const onChangeFilter = selectedYear => {
+    setSelectedYear(selectedYear);
+  };
+
+  return (
+    <div>
+      <Card className='expenses'>
+        <ExpensesFilter value={selectedYear} onChangeFilter={onChangeFilter} />
+
+        <ExpenseItem
+          title={items[0].title}
+          amount={items[0].amount}
+          date={items[0].date}
+        />
+        <ExpenseItem
+          title={items[1].title}
+          amount={items[1].amount}
+          date={items[1].date}
+        />
+        <ExpenseItem
+          title={items[2].title}
+          amount={items[2].amount}
+          date={items[2].date}
+        />
+        <ExpenseItem
+          title={items[3].title}
+          amount={items[3].amount}
+          date={items[3].date}
+        />
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
